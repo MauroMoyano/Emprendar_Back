c38f841cf3484be0ae2f6ee7fdb1e92a1971ff2d
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-02-25 19:43:20 -0300

    Armando el boilerplate

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { conectarDB } = require("./config/db");
+const cors = require("cors");
+require('dotenv').config()
+
+//creamos el servidor
+
+const app = express();
+
+// conectamos a la base de datos
+
+/* conectarDB(); */
+
+//habilitaamos cors
+const opcionesCors = {
+    origin: process.env.FRONTEND_URL,
+};
+
+app.use(cors(opcionesCors));
+
+//habilitamos leer los valores del body
+
+app.use(express.json());
+
+// dejamos definido el puerto para railway, si no existe usamos 3001
+
+const port = process.env.PORT || 3001;
+
+//definimos las rutas
+
+app.use('/', require('./routes/usuarios'))
+
+
+//arrancar la app
+
+// "0.0.0.0"  el servidor estará disponible para conexiones entrantes desde cualquier dirección IP.
+app.listen(port, "0.0.0.0", () => {
+    console.log("el servidor esta corriendo en el puerto " + port);
+});
