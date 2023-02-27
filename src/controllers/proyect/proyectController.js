1c6254ad2c54c0f08fc6ba106a65944977fb4995
Lautaro <52867567+lechugaxthz@users.noreply.github.com>
2023-02-27 18:01:52 -0300

    Update proyectController.js

--- src/controllers/proyect/proyectController.js
+++ src/controllers/proyect/proyectController.js
@@ -42,7 +42,7 @@ const getAllProyects = async () => {
         //si hay proyectos retornarlos
     if (proyects) return proyects
     //de lo contrario retornar este mensaje
-    else return 'No se encontraron proyectos'
+    else throw new Error('No se encontraron proyectos')
 }
 
 const searchProyect = async (title) => {
@@ -52,7 +52,7 @@ const searchProyect = async (title) => {
     })
 
 
-    if(!proyect) return 'No existe ningun proyecto con este nombre'
+    if(!proyect) throw new Error('No existe ningun proyecto con este nombre')
 
     return proyect
 }
