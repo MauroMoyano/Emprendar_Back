edc1874380fa8afbd4fe8225062cb451d29594c8
Sandy Pestaña <sandy.milena7@gmail.com>
2023-02-27 15:13:27 -0500

    definiendo routerUser en routes y postUser en userHandler

---
+++ src/controllers/user/userHandler.js
@@ -0,0 +1,19 @@
+const { userCreate } = requiere("../controllers");
+
+const postUser = async function (req, res) {
+  const { name, email, password, profile_img } = req.body;
+
+  try {
+    if (name && email && password && profile_img) {
+      const response = await userCreate(name, email, password, profile_img);
+      res.status(200).send(response);
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  postUser,
+};
+// {}
