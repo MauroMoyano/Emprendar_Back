d8b3d05083e80dd8ffd8d3715363e9e2ccfc659d
Nachito02 <arguellojuan08@gmail.com>
2023-03-08 14:19:15 -0300

    arreglos de crasheo

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -4,20 +4,21 @@ const { userByID } = require("../controllers/user/userController");
 const checkAuth = async (req, res, next) => {
    
         const autHeader = req.get("Authorization");
+   
       if (autHeader) {
         //obtener el token
         const token = autHeader.split(" ")[1];
         
        
         // comprobar el JWT
-    
+        
         try {
           const user = jwt.verify(token, process.env.JWT_SECRET);
     
-   
+          
           req.user = await userByID(user.id)
-       
         } catch (error) {
+          console.log(error)
          res.json('error')
         }
       }
